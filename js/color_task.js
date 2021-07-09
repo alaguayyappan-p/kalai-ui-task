186670738bb763f9fcbb75badc2e4f381b3ddb52
kalai97 <81562693+kalai97@users.noreply.github.com>
2021-07-09 22:19:55 +0530

    updated js file

--- js/color_task.js
+++ js/color_task.js
@@ -1,6 +1,13 @@
 $(".menu li").on("click",function(){
 $(".menu li").removeClass("active");
+$(".menu li").removeClass("tooltip");
 $(this).addClass("active");
+$(this).addClass("tooltip");
+var usercolor = $(this).attr("data-id");
+console.log(usercolor);
+$("[data-content-id]").hide();
+$("[data-content-id*='" + usercolor + "']").css("display","flex");
+
 })
 
 $(".background_color").spectrum({
@@ -42,5 +49,84 @@ $(".contacttext_color").spectrum({
     color:"#fb0a0c",
     change: function(color){
         $(".contact_me a").css("color","#"+color.toHex());
+    }
+});
+
+$(".getin_touch_button_color").spectrum({
+    color:"#f83300",
+    change: function(color){
+        $("a.getintouch").css("background-color","#"+color.toHex());
+    }
+});
+
+$(".getin_touch_text_color").spectrum({
+    color:"#fff",
+    change: function(color){
+        $(".getintouch").css("color","#"+color.toHex());
+    }
+});
+
+$(".website_button_color").spectrum({
+    color:"#fff",
+    change: function(color){
+        $(".visitwebsite").css("background-color","#"+color.toHex());
+    }
+});
+
+$(".website_text_color").spectrum({
+    color:"#f83300",
+    change: function(color){
+        $(".visitwebsite").css("color","#"+color.toHex());
+    }
+});
+
+$(".phoneno_color").spectrum({
+    color:"#000",
+    change: function(color){
+        $(".mobileno").css("color","#"+color.toHex());
+    }
+});
+
+$(".asidebar_color").spectrum({
+    color:"#f5cfca",
+    change: function(color){
+        $(".aside").css("background-color","#"+color.toHex());
+    }
+});
+
+// main body options
+
+$(".profile_background_color").spectrum({
+    color:"#f5cfca",
+    change: function(color){
+        $(".contrary").css("background-color","#"+color.toHex());
+    }
+});
+
+$(".body_menutext_color").spectrum({
+    color:"#f5cfca",
+    change: function(color){
+        $(".contrary").css("color","#"+color.toHex());
+    }
+});
+
+$(".profile_button_color").spectrum({
+    color:"#f62701",
+    change: function(color){
+        $(".send").css("background-color","#"+color.toHex());
+    }
+});
+
+$(".profile_text_color").spectrum({
+    color:"#fff",
+    change: function(color){
+        $(".send").css("color","#"+color.toHex());
+    }
+});
+
+$(".hyperlink_color").spectrum({
+    color:"#eaddb9",
+    change: function(color){
+        $(".page_link").css("color","#"+color.toHex());
     }
 });
